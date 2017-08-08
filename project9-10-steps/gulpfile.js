3d81b3a19da7f5ec973268ae76f0937558fc6786
Pretorian007 <pretorian007@mail.ru>
2017-08-08 17:52:43 +0300

    update portfolio page

---
+++ project9-10-steps/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var server = require('gulp-server-livereload');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('go', ['minimg', 'autocss']);
+
+gulp.task('live', function() {
+  gulp.src('')
+    .pipe(server({
+      livereload: true,
+      defaultFile: 'index.html',
+      directoryListing: false,
+      open: false
+    }));
+});
+
+
+ 
+gulp.task('minimg', () =>
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+
+gulp.task('autocss', () =>
+    gulp.src('css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'))
+);
+
+
