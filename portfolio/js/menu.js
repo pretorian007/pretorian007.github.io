accdb6bf50e7d786dce7d0a477f8045e5125e5a2
Pretorian007 <pretorian007@mail.ru>
2017-08-09 14:39:12 +0300

    updated main page

--- portfolio/js/menu.js
+++ portfolio/js/menu.js
@@ -3,7 +3,8 @@ window.onload = function(){
 	var menu = document.querySelector(".m-menu")
 	var close = document.querySelector(".close-menu")
 	var clickClose = document.querySelectorAll(".menu-link")
-
+	// var seeMore   = document.querySelector(".see-more")
+	// var more  = document.querySelector(".more")
 	link.onclick = function(){
 		menu.classList.add("m-menu_active")
 		// menu.classList.add(".m-nemu-link:hover .m-nemu-link-line:before ")
@@ -20,5 +21,21 @@ window.onload = function(){
 			menu.classList.remove("m-menu_active")
 		}
 	});
+	/*see more section*/
+	$(".more ").hide();
+	$('.see-more').click(function(){
+	$(".more ").slideToggle();
+	});
+	// seeMore .onclick = function () {
+	// 	more.classList.toggle("more-active");
+	// }
 
 }
+
+
+
+$(document).ready(function(){
+
+
+
+});
