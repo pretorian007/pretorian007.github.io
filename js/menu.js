14ee31ae71e738f5632c2898124fbcc86905a29d
Pretorian007 <pretorian007@mail.ru>
2017-08-23 01:07:04 +0300

    upd mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,60 @@
-window.onload = function(){
-	var link = document.querySelector(".m-nemu-link")
-	var menu = document.querySelector(".m-menu")
-	var close = document.querySelector(".close-menu")
-	var clickClose = document.querySelectorAll(".menu-link")
+$( document ).ready(function() {
+var menu = $(".menu");
+var link = $(".m-nemu-link");
+var close = $(".close-menu");
+var clickClose = $(".menu-link")
 
-	link.onclick = function(){
-		menu.classList.add("m-menu_active")
-		console.log ("opend")
-	}
-	close.onclick = function(){
-		menu.classList.remove("m-menu_active")
-		console.log ("closed")
-	}
+
+		link.click(function(){
+			menu.removeClass("menu").addClass("m-menu")
+			close.css("display","block")
+		})
+
+		close.click(function(){
+			menu.removeClass("m-menu ").addClass("menu")
+			close.css("display","none")
+	
+		})
+
+		clickClose.click(function(){
+			menu.removeClass("m-menu ").addClass("menu")	
+			close.css("display","none")	
+		})
+		//SEE MORE BLOCK
+	$(".more ").hide();
+	$('.see-more').click(function(){
+	$(".more ").slideToggle();
+	});
+
+
+});
+
+
+
+
+
+
+
+
+
+// window.onload = function(){
+// 	var link = document.querySelector(".m-nemu-link")
+// 	var menu = document.querySelector(".menu")
+// 	var close = document.querySelector(".close-menu")
+// 	var clickClose = document.querySelectorAll(".menu-link")
+
+// 	link.onclick = function(){
+// 		menu.classList.remove("menu")
+// 		menu.classList.add("m-menu", "m-menu_active")
+// 		close.style.display ="block";
+// 		console.log ("opend")
+// 	}
+// 	close.onclick = function(){
+// 		menu.classList.remove("m-menu", "m-menu_active");
+// 		menu.classList.add("menu")
+// 		close.style.display ="none";
+// 		console.log ("closed")
+// 	}
 
 
 	clickClose.forEach(function (el) {
@@ -19,14 +62,15 @@ window.onload = function(){
 			menu.classList.remove("m-menu_active")
 		}
 	});
-	/*see more section*/
-	$(".more ").hide();
-	$('.see-more').click(function(){
-	$(".more ").slideToggle();
-	});
-	/*PRELOADER*/
- // var $preloader = $('#p_prldr'),
- //    $svg_anm = $preloader.find('.svg_anm');
- //    $preloader.delay(1400).fadeOut('slow');
-}
+
+
+
+
+
+
+// 	/*PRELOADER*/
+//  // var $preloader = $('#p_prldr'),
+//  //    $svg_anm = $preloader.find('.svg_anm');
+//  //    $preloader.delay(1400).fadeOut('slow');
+// }
 
