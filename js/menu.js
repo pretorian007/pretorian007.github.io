e611aa5a080baee97531ef0e8274f5c880ad75a5
Pretorian007 <pretorian007@mail.ru>
2017-08-22 16:41:09 +0300

    upd main project

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,9 @@ window.onload = function(){
 	var menu = document.querySelector(".m-menu")
 	var close = document.querySelector(".close-menu")
 	var clickClose = document.querySelectorAll(".menu-link")
-	// var seeMore   = document.querySelector(".see-more")
-	// var more  = document.querySelector(".more")
+
 	link.onclick = function(){
 		menu.classList.add("m-menu_active")
-		// menu.classList.add(".m-nemu-link:hover .m-nemu-link-line:before ")
 		console.log ("opend")
 	}
 	close.onclick = function(){
@@ -26,12 +24,5 @@ window.onload = function(){
 	$('.see-more').click(function(){
 	$(".more ").slideToggle();
 	});
-	// seeMore .onclick = function () {
-	// 	more.classList.toggle("more-active");
-	// }
-// var oopps = document.querySelector(".oops")
-// oopps.onclick = function(){
-// 		console.log ("Oops! Эта страничка еще в разработке");
-// 	};
-}
 
+}
